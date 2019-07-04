3ef38a00e9cb4722d1eb115f283ae74dcd9f56cf
zhanglucheng <zhanglucheng@houbank.com>
2019-07-04 14:54:44 +0800

    Updates

---
+++ precache-manifest.4a4dd0440af8ebc3918273bb4164ee63.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "cad9d22d866fe7d1d456",
+    "url": "./static/js/8.f2e020a6.chunk.js"
+  },
+  {
+    "revision": "a72b4cbb33894e0ea63b",
+    "url": "./static/js/0.63e5406d.chunk.js"
+  },
+  {
+    "revision": "44dc3bc6a8095742e270",
+    "url": "./static/js/2.ba6fa2fd.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "68aacb269458a962680c",
+    "url": "./static/js/main.0ba51fc4.chunk.js"
+  },
+  {
+    "revision": "d82be6c85d33570ca42a",
+    "url": "./static/js/runtime~main.ebfca277.js"
+  },
+  {
+    "revision": "67565eb175e6d96a8bec",
+    "url": "./static/js/5.1e6a37db.chunk.js"
+  },
+  {
+    "revision": "f076e388a63981e2a15c",
+    "url": "./static/js/16.240aaab7.chunk.js"
+  },
+  {
+    "revision": "edd31c8961d676e2285d",
+    "url": "./static/js/6.a62d4610.chunk.js"
+  },
+  {
+    "revision": "9abe64510e8648c3744f",
+    "url": "./static/js/15.eb882698.chunk.js"
+  },
+  {
+    "revision": "49ed892f60163d9f7a9c",
+    "url": "./static/js/7.12e2c437.chunk.js"
+  },
+  {
+    "revision": "b8f2ec7c985d2f19fa34",
+    "url": "./static/js/1.4e1d7b05.chunk.js"
+  },
+  {
+    "revision": "cd281bf6860651992585",
+    "url": "./static/js/9.a975ba02.chunk.js"
+  },
+  {
+    "revision": "616729dd508580aa7560",
+    "url": "./static/js/10.ccbade7b.chunk.js"
+  },
+  {
+    "revision": "fffa0bf538a84246ed6a",
+    "url": "./static/js/11.3a03a25d.chunk.js"
+  },
+  {
+    "revision": "2426c352c3116715b16f",
+    "url": "./static/js/12.03cea1ac.chunk.js"
+  },
+  {
+    "revision": "966a5207ea50fd9f626e",
+    "url": "./static/js/13.60d362e1.chunk.js"
+  },
+  {
+    "revision": "50dd50bad9e445e55849",
+    "url": "./static/js/14.114cce2b.chunk.js"
+  },
+  {
+    "revision": "68aacb269458a962680c",
+    "url": "./static/css/main.179e5f2a.chunk.css"
+  },
+  {
+    "revision": "49ed892f60163d9f7a9c",
+    "url": "./static/css/7.dcbd5c95.chunk.css"
+  },
+  {
+    "revision": "edd31c8961d676e2285d",
+    "url": "./static/css/6.7bbb1312.chunk.css"
+  },
+  {
+    "revision": "105a66ccd75850e171fc2cd108687961",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-admin-app/precache-manifest.184db4cc38b0f1e0fa9ce89f616acd4a.js"
+  "./precache-manifest.4a4dd0440af8ebc3918273bb4164ee63.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-admin-app/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
